d47c29dbccef05f6b49449ee6aeb6ac9bbf8ce25
Amir Taheri <amir.houshang.taheri@gmail.com>
2018-03-22 02:04:24 +0100

    url changed

--- scripts/api.js
+++ scripts/api.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     var numberOfSections = 3;
     var jsonData = null;
     var col = [];
-    var url = "scripts/citylist.json";
+    var url = "https://amirhtaheri.github.io/Weather/scripts/citylist.json";
     var _url = "https://amirhtaheri.github.io/Auction/scripts/auction.json";
     var urlOld = "https://nackowskis.azurewebsites.net/api/Auktion/200/";
 
