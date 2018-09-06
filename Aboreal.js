0f4f6e2d84a239c7a285d06c5a2727c35717e155
amirht <amir.houshang.taheri@gmail.com>
2018-09-06 20:43:34 +0200

    Some files deleted

--- Aboreal.js
+++
@@ -1,8 +0,0 @@
-﻿function navToggle() {
-    var x = document.getElementById("content-toggle");
-    if (x.className === "navigation-menu") {
-        x.className += " responsive";
-    } else {
-        x.className = "navigation-menu";
-    }
-}
